ef869f771ce14aea4c5750f024f4170aa0f1df1a
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-08-31 17:40:24 +0530

    Functionality and Test Cases for Adding-getting user are implemented

--- library.py
+++ library.py
@@ -6,7 +6,14 @@ class Book:
         self.publication_year = publication_year
         self.availability = True
 
+class User:
+    def __init__(self, userId, name):
+        self.userId = userId
+        self.name = name
+        self.borrowed_books = []
+
 books_list = []
+users_list = []
 
 # All Functionalities
 
@@ -14,11 +21,41 @@ def get_books():
     """To get all books of book list"""
     return books_list
 
+def get_users():
+    """To get all users of user list"""
+    return users_list
+
+
+def add_user(user):
+    """To add user in the user list of library"""
+
+    if not user.userId:
+        raise ValueError("userId must be provided")
+    
+    if not user.name:
+        raise ValueError("user name must be provided")
+    
+    for u in users_list:
+        if u.userId == user.userId:
+            raise ValueError("user with given userId already exists") 
+        
+    users_list.append(user)
+
+
 def add_book(book):
     """To add Book in the book list"""
 
-    if not book.ISBN or not book.title or not book.author or not book.publication_year:
-        raise ValueError("All book details must be provided")
+    if not book.ISBN:
+        raise ValueError("ISBN must be provided")
+    
+    if not book.title:
+        raise ValueError("Title of the book must be provided")
+    
+    if not book.author:
+        raise ValueError("Author of the book must be provided")
+    
+    if not book.publication_year:
+        raise ValueError("Publication Year of the book must be provided")
 
     for existing_book in books_list:
         if existing_book.ISBN == book.ISBN:
@@ -26,13 +63,29 @@ def add_book(book):
 
     books_list.append(book)
 
-def borrow_book(ISBN):
+def borrow_book(userId, ISBN):
     """To borrow the book if available"""
+    
+    if not userId or userId == "":
+        raise ValueError("userId is missing")
+    
+    if not ISBN or ISBN == "":
+        raise ValueError("ISBN is missing")
+    
+    user = None
+    
+    for u in users_list:
+        if u.userId == userId:
+            user = u
+
+    if not user:
+        raise ValueError("User does not exist with given userId")
 
     for book in books_list:
         if book.ISBN == ISBN:
             if book.availability:
                 book.availability = False
+                user.borrowed_books.append(book)
                 return True
             else:
                 raise ValueError("Book is already borrowed")
--- test_library.py
+++ test_library.py
@@ -1,5 +1,31 @@
 import pytest
-from library import add_book, borrow_book, get_books, Book
+from library import add_book, borrow_book, get_books, add_user, get_users, Book, User
+
+# Test Cases for Adding User
+
+def test_add_user():
+    """Adding a user should be successful"""
+    old_len = len(get_users())
+    user = User("01", "Parth")
+    add_user(user)
+    users_list = get_users()
+    assert len(users_list) == old_len + 1
+    assert users_list[len(users_list) - 1].name == "Parth"
+
+def test_add_user_with_missing_userId():
+    """Missing userId of the user while adding should raise ValueError"""
+    with pytest.raises(ValueError):
+        add_user(User("", "Parth"))
+
+def test_add_user_with_missing_name():
+    """Missing name of the user while adding should raise ValueError"""    
+    with pytest.raises(ValueError):
+        add_user(User("01", ""))
+
+def test_add_user_with_duplicate_userId():
+    """Adding a user with a duplicate userId should raise ValueError"""
+    with pytest.raises(ValueError):
+        add_user(User("01", "Parth"))
       
 # Test Cases for Adding Book
 
@@ -12,19 +38,28 @@ def test_add_book():
     assert len(books_list) == old_len + 1
     assert books_list[len(books_list) - 1].title == "The Hitchhiker's Guide to the Galaxy"
 
-def test_add_book_with_duplicate_isbn():
+def test_add_book_with_duplicate_ISBN():
     """Adding a book with a duplicate ISBN should raise ValueError"""
     with pytest.raises(ValueError):
         add_book(Book("0001", "Another Book", "Different Author", 2023))
 
-def test_add_book_with_missing_required_fields():
-    """Missing book details should raise ValueError"""
+def test_add_book_with_missing_ISBN():
+    """Missing ISBN of the book while adding should raise ValueError"""
     with pytest.raises(ValueError):
         add_book(Book("", "Title", "Author", 2023))
+
+def test_add_book_with_missing_title():
+    """Missing Title of the book while adding should raise ValueError"""    
     with pytest.raises(ValueError):
         add_book(Book("0001", "", "Author", 2023))
+
+def test_add_book_with_missing_author():
+    """Missing Author of the book while adding should raise ValueError"""
     with pytest.raises(ValueError):
         add_book(Book("0001", "Title", "", 2023))
+
+def test_add_book_with_missing_publication_year():
+    """Missing Publication Year of the book while adding should raise ValueError"""
     with pytest.raises(ValueError):
         add_book(Book("0001", "Title", "Author", ""))
         
@@ -33,15 +68,30 @@ def test_add_book_with_missing_required_fields():
 def test_borrow_book():
     """Borrowing a book should be successful"""
     books_list = get_books()
-    assert borrow_book("0001")
+    assert borrow_book("01", "0001")
     assert not books_list[0].availability
 
-def test_borrow_book_multiple_times():
+def test_borrow_book_without_userId():
+    """Borrowing a book without userId should raise an error"""
+    with pytest.raises(ValueError):
+        borrow_book("", "0001")
+
+def test_borrow_book_without_ISBN():
+    """Borrowing a book without ISBN should raise an error"""
+    with pytest.raises(ValueError):
+        borrow_book("01", "")
+    
+def test_borrow_book_not_available():
     """Borrowing a book that is already borrowed (not available) should raise an error"""
     with pytest.raises(ValueError):
-        borrow_book("0001")
+        borrow_book("01", "0001")
+
+def test_borrow_book_nonexistent_user():
+    """Borrowing a book with user that does not exist (Incorrect userId) should raise an error"""
+    with pytest.raises(ValueError):
+        borrow_book("05", "0001")
         
 def test_borrow_nonexistent_book():
     """Borrowing a book that does not exist should raise an error"""
     with pytest.raises(ValueError):
-        borrow_book("9876")
+        borrow_book("01", "9876")
